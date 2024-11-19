42d69daa3868775f75fb0a31fc036648e0a8ba68
jhonvillanueva44 <jhon.villanueva.l@tecsup.edu.pe>
2024-11-19 05:20:08 -0500

    doceavo commit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 MEDIA_URL = '/media/'
